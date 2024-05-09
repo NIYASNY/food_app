1c4c6b40f20e837e7a1cf317f18d93b21da99654
Niyas Ny <niyasny433@gmail.com>
2024-05-09 18:13:33 +0530

    second commit

---
+++ packages/user_repository/lib/src/models/models.dart
@@ -0,0 +1,39 @@
+export 'user.dart';
+
+class MyUser {
+  String userId;
+  String email;
+  String name;
+  bool isActivecart;
+
+  MyUser(
+      {required this.userId,
+      required this.email,
+      required this.name,
+      required this.isActivecart});
+
+  static final empty =
+      MyUser(userId: '', email: '', name: '', isActivecart: false);
+
+  MyUserEntity toEntity() {
+    return MyUserEntity(
+      userId: userId,
+      email: email,
+      name: name,
+      isActivecart: isActivecart,
+    );
+  }
+
+  static MyUser fromEntity(MyUserEntity entity) {
+    return MyUser(
+        userId: entity.userId,
+        email: entity.email,
+        name: entity.name,
+        isActivecart: entity.isActivecart);
+  }
+
+  @override
+  String toString() {
+    return "MyUser: $userId, $email, $name, $isActivecart";
+  }
+}
