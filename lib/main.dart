cf7d4e2b98258288e7f172b565aa767343f1c08b
Niyas Ny <niyasny433@gmail.com>
2024-05-10 17:56:20 +0530

    auth page started

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:foodapp/app.dart';
+import 'package:foodapp/simple_bloc_obsever.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
 }
-
-
--- lib/simple_bloc_obsever.dart
+++ lib/simple_bloc_obsever.dart
@@ -1,6 +1,45 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 
 class SimpleBlocObserver extends BlocObserver {
+
+  //......onCreate.......//
   @override
-  void onCreate(BlocBase bloc) {}
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    log('onCreate -- bloc: ${bloc.runtimeType}');
+  }
+
+   //......onEvent.......//
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    log('onEvent -- bloc: ${bloc.runtimeType}, event: $event');
+  }
+
+
+   //......onChange.......//
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log('onChange -- bloc: ${bloc.runtimeType}, change: $change');
+  }
+
+   //......onTransition.......//
+   void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    log('onTransition -- bloc: ${bloc.runtimeType}, onTransition: $transition');
+  }
+
+  //......onError.......//
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    log('onError -- bloc: ${bloc.runtimeType}, error: $error');
+  }
+
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    log('onClose -- bloc: ${bloc.runtimeType}');
+  }
 }
