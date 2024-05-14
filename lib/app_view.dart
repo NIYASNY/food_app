d105766bb9ca59cef157bb58a2a70af58d01bf9e
Niyas Ny <niyasny433@gmail.com>
2024-05-14 17:36:40 +0530

    started building bloc on appview page

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodapp/blocs/bloc/auth_bloc_bloc.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Food Delivary',
+        theme: ThemeData(
+            colorScheme: ColorScheme.light(
+                background: Colors.grey.shade100,
+                onBackground: Colors.black,
+                primary: Colors.blue,
+                onPrimary: Colors.white)),
+        home: BlocBuilder<AuthBlocBloc, AuthBlocState>(
+            builder: ((context, state) {
+          if (state.status == AuthBlocStatus.authenticated) {
+            return HomeScreen();
+          } else {
+            return WelcomeScreen();
+          }
+        }),),);
   }
 }
